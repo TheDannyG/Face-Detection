6282c6fdc2d2070322786f9d0b7a76ab035860de
vgvgvg <vgrichanov@gmail.com>
2017-02-18 12:12:11 -0500

    Create collection initial version

---
+++ create_collection.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 21:05:30 2017
+
+@author: dad
+"""
+import boto3
+
+#connect to AWS
+client = boto3.client('rekognition', region_name='us-east-1')
+
+#collection name
+collection_name = 'authorized_faces'
+
+response_list = client.list_collections()
+if collection_name not in response_list['CollectionIds']: 
+    #create collection
+    response_coll = client.create_collection(
+        CollectionId = collection_name
+    )
+    print response_coll
+else: 
+    print 'Collection ', collection_name, ' already exists.'
+    response_faces = client.list_faces(CollectionId=collection_name)
+    print collection_name, 'contains ', len(response_faces['Faces']), ' faces'
+    print response_faces['Faces']
+
+
+
+
