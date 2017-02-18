f7f813f5951db5b6b33bfd2f9cad8bb4e3bc4ac7
root32-01 <dggrishanov007@gmail.com>
2017-02-17 19:47:29 -0500

    Update s3test.py

--- s3test.py
+++ s3test.py
@@ -10,8 +10,8 @@ import botocore
 client = boto3.resource(
         's3',
         region_name = 'us-east-1', 
-        aws_access_key_id = 'AKIAJSBBWLF7LNQYLUOQ',
-        aws_secret_access_key = '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',
+        aws_access_key_id = 'accesskey',
+        aws_secret_access_key = 'secretaccesskey',
 )
 
 bucket = s3.Bucket('Rekognition')
