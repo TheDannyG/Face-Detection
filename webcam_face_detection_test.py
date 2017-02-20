b28e973fe21dcb45301e381351ec7bffecfcaa69
vgvgvg <vgrichanov@gmail.com>
2017-02-19 20:25:20 -0500

    Comments added

--- webcam_face_detection_test.py
+++ webcam_face_detection_test.py
@@ -1,3 +1,7 @@
+#Detects faces realtime using OpenCV and uses AWS Rekognition to identify people agianst face collection stored in AWS
+# needs search_faces.py
+#============================================================================================
+
 import cv2
 import sys
 import logging as log
@@ -5,10 +9,9 @@ import datetime as dt
 
 from time import sleep
 import boto3
-#import index_face function
 from search_faces import search_face 
 
-#cascPath = sys.argv[1]
+#Create classifier
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 log.basicConfig(filename='webcam.log',level=log.INFO)
 
@@ -37,12 +40,14 @@ while True:
 	
 	frame_cnt = frame_cnt + 1
 	
-   
+	# Check for faces every 10 frames
 	if frame_cnt == 10:
 		frame_cnt = 0
 
+		#convert into grey
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+		#detect faces
 		faces = faceCascade.detectMultiScale(
 			gray,
 			scaleFactor=1.1,
@@ -54,22 +59,23 @@ while True:
 	# Draw a rectangle around the faces
 	for (x, y, w, h) in faces:
 		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-		# Write some Text
+		# Write face name
 		font = cv2.FONT_HERSHEY_SIMPLEX
 		cv2.putText(frame,face_name,(x,y), font, 1,(0,255,0),2)
 
-
-		
-
+	# write log
 	if anterior != len(faces):
 		anterior = len(faces)
 		log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
 
+	#Read keys
 	key = cv2.waitKey(1) & 0xFF    
 
+	#Quit on 'q'
 	if key == ord('q'):
 		break
 	
+	#Detect face on 'd'
 	if key == ord('d'):
 		face_name = search_face(client, cv2.imencode('.jpg', frame)[1].tostring(), collection_name, match_threshold)
 		print (face_name)
