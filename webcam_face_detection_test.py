e040c1b09e9f9273431e999ced1b59eb69a362dc
vgvgvg <vgrichanov@gmail.com>
2017-02-19 20:04:59 -0500

    Finds faces on webcam stream and identifies people using AWS Rekognition

---
+++ webcam_face_detection_test.py
@@ -0,0 +1,82 @@
+import cv2
+import sys
+import logging as log
+import datetime as dt
+
+from time import sleep
+import boto3
+#import index_face function
+from search_faces import search_face 
+
+#cascPath = sys.argv[1]
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+log.basicConfig(filename='webcam.log',level=log.INFO)
+
+video_capture = cv2.VideoCapture(0)
+anterior = 0
+frame_cnt = 0
+faces = []
+face_name = ''
+
+#connect to AWS
+client = boto3.client('rekognition')
+
+#set collection name
+collection_name = 'authorized_faces'
+match_threshold = 0.9
+
+
+while True:
+	if not video_capture.isOpened():
+		print('Unable to load camera.')
+		sleep(5)
+		pass
+
+	# Capture frame-by-frame
+	ret, frame = video_capture.read()
+	
+	frame_cnt = frame_cnt + 1
+	
+   
+	if frame_cnt == 10:
+		frame_cnt = 0
+
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+		faces = faceCascade.detectMultiScale(
+			gray,
+			scaleFactor=1.1,
+			minNeighbors=5,
+			minSize=(30, 30)
+		)
+
+
+	# Draw a rectangle around the faces
+	for (x, y, w, h) in faces:
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+		# Write some Text
+		font = cv2.FONT_HERSHEY_SIMPLEX
+		cv2.putText(frame,face_name,(x,y), font, 1,(0,255,0),2)
+
+
+		
+
+	if anterior != len(faces):
+		anterior = len(faces)
+		log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
+	key = cv2.waitKey(1) & 0xFF    
+
+	if key == ord('q'):
+		break
+	
+	if key == ord('d'):
+		face_name = search_face(client, cv2.imencode('.jpg', frame)[1].tostring(), collection_name, match_threshold)
+		print (face_name)
+	
+	# Display the resulting frame
+	cv2.imshow('Video', frame)
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
